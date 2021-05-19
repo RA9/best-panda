15e95afa2e962e4dc468c2e23fd25fef5cc5a85f
mrhumble <carlos@kwagei.com>
2021-05-19 06:38:51 +0000

    change footer copyright text

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>A GatsbyJS Starter for Netlify CMS, Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Stackrole.com</Link></p>
+      <p>Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="https://twitter.com/rademejs">Carlos S. Nah</Link></p>
     </div>
   </footer>
 )
